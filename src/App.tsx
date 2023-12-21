a2999980bbe15dadaf31549b10046b8610b7814e
mahywr <mahywr.yt@gmail.com>
2023-12-21 18:20:34 +0000

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,24 @@
 import ListGroup from "./components/ListGroup";
+import { Fragment } from "react";
 
 function App() {
+  const items = ["new york", "tehran", "leeds", "peterzburg", "mazandaran"];
   return (
-    <ul className="list-group">
-      <li className="list-group-item">An item</li>
-      <li className="list-group-item">A second item</li>
-      <li className="list-group-item">A third item</li>
-      <li className="list-group-item">A fourth item</li>
-      <li className="list-group-item">And a fifth one</li>
-    </ul>
+    <>
+      <h1>List</h1>
+      {items.length === 0 && <p>No item found</p>}
+      <ul className="list-group">
+        {items.map((item, index) => (
+          <li
+            className="list-group-item"
+            key={item}
+            onClick={() => console.log(item, index)}
+          >
+            {item}
+          </li>
+        ))}
+      </ul>
+    </>
   );
 }
 export default App;
